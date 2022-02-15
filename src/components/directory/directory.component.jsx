1419ed9ebe2dfd4506fcf0ea52495236cf12c026
Lucas Tannús <lucastannus@Lucas-Tannus-MacBook-Pro-295A.local>
2022-02-15 10:06:30 -0300

    feat: start new project to improve React framework

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+import "./directory.styles.scss"
+import SECTIONS_DATA from "./sections.data"
+import MenuItem from "../menu-item/menu-item.component"
+
+class Directory extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            sections: SECTIONS_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                { this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Directory
