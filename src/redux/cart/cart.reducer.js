5c8bfc2f88cca52d8c8d2c3cf2650a6b17ae63d2
Lucas Tannus <lucastannusvieira@gmail.com>
2022-02-17 11:08:40 -0300

    feat: adding item to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,8 @@
 import { CartActionTypes } from "./cart.types"
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cart_items: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -9,6 +10,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 hidden: !state.hidden
+            }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cart_items: [...state.cart_items, action.payload]
             }
         default:
             return state
