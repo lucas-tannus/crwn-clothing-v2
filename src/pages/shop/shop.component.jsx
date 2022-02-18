e74cf232e548a18019fe36dfc9d3d2a530d01bee
Lucas Tannus <lucastannusvieira@gmail.com>
2022-02-18 18:37:37 -0300

    feat: add shop redux and refactor shop page components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,13 @@
 import React from "react"
 
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component"
+
 import "./shop.styles.scss"
 
-import SHOP_DATA from "./shop.data.js"
-import CollectionPreview from "../../components/collection-preview/collection-preview.component"
-
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <div key={ id }>
-                            <CollectionPreview { ...otherCollectionProps } />
-                        </div>
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const ShopPage = () => (
+    <div className="shop-page">
+        <CollectionsOverview />        
+    </div>
+)
 
 export default ShopPage
